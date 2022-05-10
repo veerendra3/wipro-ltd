d66ff4613ebb1dc520d01e0ce1a3daf227e7f62e
veerendra3 <kvvchari2001@gmail.com>
2022-05-10 13:05:52 +0530

    Final Commit 1.0

---
+++ src/app/data.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Account } from './account';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http : HttpClient) { }
+
+  helloService(){
+    return "Angular Service"
+  }
+
+  getUsers() {
+    return this.http.get('https://reqres.in/api/users')
+  }
+
+  baseUrl='http://localhost:9005/user';
+  getAccounts(employeeId:number):Observable<Account> 
+  {
+    return this.http.get<Account>(`${this.baseUrl}/${employeeId}`);
+  }
+
+}
---
+++ src/app/movie.ts
@@ -0,0 +1,7 @@
+export class Movie {
+
+    title : string |any;
+    director : string |any;
+    cast : string |any;
+    releaseDate : string |any;
+}
